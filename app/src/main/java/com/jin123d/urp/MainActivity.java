9d10421ebdeaf39057f7796174360ed9a4574fd1
jin123d <jin123d@jin123d.com>
2016-01-04 19:23:24 +0800

    友盟升级

--- app/src/main/java/com/jin123d/urp/MainActivity.java
+++ app/src/main/java/com/jin123d/urp/MainActivity.java
@@ -32,6 +32,7 @@ import com.jin123d.models.MainModels;
 import com.jin123d.util.Sp;
 import com.jin123d.util.netUtil;
 import com.jin123d.util.urlUtil;
+import com.umeng.update.UmengUpdateAgent;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -203,6 +204,9 @@ public class MainActivity extends AppCompatActivity implements GetNetData {
                                 start(LoginActivity.class);
                             }
                         }).create().show();
+                        break;
+                    case R.id.item_update:
+                        UmengUpdateAgent.forceUpdate(MainActivity.this);
                         break;
                 }
                 return false;
